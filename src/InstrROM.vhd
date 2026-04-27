aec27af2ee449dfe8714f3e8b8147c72f2246f47
MohammadHosseinKv <mohammadhossein.kv@gmail.com>
2026-04-27 16:54:24 +0330

    separate instruction rom itself and intrustion fetch logic

--- src/InstrROM.vhd
+++ src/InstrROM.vhd
@@ -2,11 +2,11 @@ library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
   use work.Arma2_Consts.all;
+  use work.instr_rom_init.all;
 
 entity InstrROM is
   generic (
     INSTR_DATA_WIDTH : integer := C_INSTR_DATA_WIDTH; -- 16 bit instruction
-    CAPACITY         : integer := C_MEM_CAPACITY;     -- 512 byte
     ADDR_WIDTH       : integer := C_MEM_ADDR_WIDTH    -- 2 byte = 16 bit = 1 word
   );
   port (
@@ -16,12 +16,6 @@ entity InstrROM is
 end entity;
 
 architecture behaviour of InstrROM is
-  type rom_t is array (0 to (CAPACITY / INSTR_DATA_WIDTH) - 1) of std_logic_vector(INSTR_DATA_WIDTH - 1 downto 0);
-  constant ROM_CONTENT : rom_t := (
-    0      => x"A105",
-    1      => x"A20A",
-    others => (others => '0')
-  );
 begin
   instr <= ROM_CONTENT(to_integer(unsigned(addr)));
 end architecture;
